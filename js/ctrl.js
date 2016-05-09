443a9554e5ff76dd235f2e0ec5791b2693803c18
Trevor <scipatterns@gmail.com>
2016-05-08 21:00:55 -0600

    good start

--- js/ctrl.js
+++ js/ctrl.js
@@ -4,33 +4,7 @@
     app.controller("myCtrl", function ($scope) {
 
 
-        $scope.top_dir = {
-            "Math": {
-                "Calculus": {}
-            },
-            "Statistics": {
-                "Math 5080": {}
-            },
-            "Programming": {
-                "Python": {},
-                "R": {
-                    "RGtk2": {
-                        "basic": {
-                            "button": "basic/button.html",
-                            "Window": "basic/window.html"}
-                    }
-                },
-                "SAS": {},
-                "SPSS": {},
-                "Excel": {}
-            },
-            "Work": {
-                "Time": {},
-                "Projects": {},
-                "Notes": {}
-            }
-
-        };
+        $scope.top_dir =tree;
 
 
         $scope.active_1 = "Home";
@@ -38,6 +12,7 @@
         $scope.package_active = false;
         $scope.topic_active = false;
         $scope.sub_topic_active = false;
+        $scope.base_active = false;
 
 
         $scope.active_1_set = function (file_name) {
@@ -46,6 +21,7 @@
             $scope.package_active = false;
             $scope.topic_active = false;
             $scope.sub_topic_active = false;
+            $scope.base_active = false;
 
 
         };
@@ -55,6 +31,7 @@
             $scope.package_active = false;
             $scope.topic_active = false;
             $scope.sub_topic_active = false;
+            $scope.base_active = false;
 
 
         };
@@ -63,17 +40,25 @@
             $scope.package_active = file_name;
             $scope.topic_active = false;
             $scope.sub_topic_active = false;
+            $scope.base_active = false;
 
 
         };
         $scope.active_topic_set = function (file_name) {
             $scope.topic_active = file_name;
             $scope.sub_topic_active = false;
+            $scope.base_active = false;
 
 
         };
         $scope.active_sub_topic_set = function (file_name) {
             $scope.sub_topic_active = file_name;
+            $scope.base_active = false;
+
+
+        };
+        $scope.active_base_set = function (file_name) {
+            $scope.base_active = file_name;
 
 
         };
---
+++ js/tree.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Trevor on 5/8/2016.
+ */
+tree = {
+    "Math": {
+        "Calculus": {}
+    },
+    "Statistics": {
+        "Math 5080": {}
+    },
+    "Programming": {
+        "Python": {},
+        "R": {
+            "RGtk2": {
+                "basic": {
+                    "button": {
+                        "index": "Programming/R/RGtk2/basic/button/button.html",
+                        "index2": "Programming/R/RGtk2/basic/button/button.html",
+                        "index3": "Programming/R/RGtk2/basic/button/button.html",
+                        "index4": "Programming/R/RGtk2/basic/button/button.html"
+                    },
+                    "Window": {
+                        "index": "Programming/R/RGtk2/basic/window/window.html"
+                    }
+                }
+            }
+        },
+        "SAS": {},
+        "SPSS": {},
+        "Excel": {}
+    }
+
+
+};
