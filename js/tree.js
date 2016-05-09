efc4daddd0cd9e111ff4aa9d70ccbfc1edcadae6
Trevor <scipatterns@gmail.com>
2016-05-08 21:52:03 -0600

    updated index chapters for RGtk

--- js/tree.js
+++ js/tree.js
@@ -12,6 +12,18 @@ tree = {
         "Python": {},
         "R": {
             "RGtk2": {
+                "index":{
+                    "index":{
+                        "index": "Programming/R/RGtk2/index/index/index.html",
+                        "chapter 6": "Programming/R/RGtk2/index/index/chapter6.html",
+                        "chapter 7": "Programming/R/RGtk2/index/index/chapter7.html",
+                        "chapter 8": "Programming/R/RGtk2/index/index/chapter8.html",
+                        "chapter 9": "Programming/R/RGtk2/index/index/chapter9.html",
+                        "chapter 10": "Programming/R/RGtk2/index/index/chapter10.html",
+                        "chapter 11": "Programming/R/RGtk2/index/index/chapter11.html",
+
+                    }
+                },
                 "basic": {
                     "button": {
                         "index": "Programming/R/RGtk2/basic/button/button.html",
